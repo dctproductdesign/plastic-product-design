153adaab2736c39c18f429c55db301c3dbd748cf
divya <divya@gmail.com>
2026-02-01 20:21:19 +0530

    all set

--- src/Components/Pricingoffer/offerSchedule.jsx
+++ src/Components/Pricingoffer/offerSchedule.jsx
@@ -2,10 +2,10 @@ export const OFFER = {
   // Change this whenever you want (tomorrow 2PM etc.)
   // Format must be ISO with timezone offset for India:
   // YYYY-MM-DDTHH:mm:ss+05:30
-  startsAt: "2026-02-01T20:00:00+05:30",
+  startsAt: "2026-02-01T20:30:00+05:30",
 
   // strict 2 hours
-  durationMinutes: 60,
+  durationMinutes: 90,
 
   // prices
   offerPrice: 12999,
