6ae870f89099a48ce50cd70da4f8288693b6486d
divya <divya@gmail.com>
2026-02-08 18:44:09 +0530

    all set

--- src/App.jsx
+++ src/App.jsx
@@ -488,7 +488,7 @@ export default function Landing() {
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75" />
                 <span className="relative inline-flex rounded-full h-2 w-2 bg-sky-500" />
               </span>
-              Enrollments Closing Today - Batch Starts Tomorrow|2nd February 2026!
+              Registrations Closing Today - Batch Starts Tomorrow|9th February 2026!
             </span>
           </div>
 
